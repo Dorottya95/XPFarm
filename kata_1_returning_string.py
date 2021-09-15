e091ed576453aef7604b0e3c569cbf0f5e188c93
dorottya-szalai1 <dorottya.szalai1@ibm.com>
2021-09-15 21:47:10 +0200

    Returning Strings 2

--- kata_1_returning_string.py
+++ kata_1_returning_string.py
@@ -1,4 +1,7 @@
 def greet(name):
-    #Good Luck (like you need it)
-    pass
+    if check_string(name):
+        return print("Hello, " + name + " how are you doing today?")
 
+
+def check_string(input):
+    return print(isinstance(input, str))
--- test_kata_1_returning_string.py
+++ test_kata_1_returning_string.py
@@ -1,11 +1,13 @@
 import unittest as test
-from kata_1_returning_string import greet
+from kata_1_returning_string import greet, check_string
 
 
 class TestString(test.TestCase):
     def basic_test_cases():
         test.assert_equals(greet('Ryan'), "Hello, Ryan how are you doing today?")
         test.assert_equals(greet('Shingles'), "Hello, Shingles how are you doing today?")
+    def check_if_string(self):
+        self.assertEqual(check_string('string'), True)
 
 if __name__ == '__main__':
     test.main()
