1818c859f9f522f81a3c8ae3d81fa8ba9f06f08a
dorottya-szalai1 <dorottya.szalai1@ibm.com>
2021-09-18 17:41:23 +0200

    Remove 1st and last char 1

---
+++ 2. Remove First and Last Character KATA/test.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_remove_char(self):
+        self.fail()
+
+'''
+import codewars_test as test
+from solution import remove_char
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(remove_char('eloquent'), 'loquen')
+        test.assert_equals(remove_char('country'), 'ountr')
+        test.assert_equals(remove_char('person'), 'erso')
+        test.assert_equals(remove_char('place'), 'lac')
+        test.assert_equals(remove_char('ok'), '')
+        test.assert_equals(remove_char('ooopsss'), 'oopss')
+'''
