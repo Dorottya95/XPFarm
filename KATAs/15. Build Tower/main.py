274d69de93979d4dd25309036fbf9b12ad127b01
Dorka <dorottya.szalai1@ibm.com>
2021-10-26 20:06:14 +0200

    build tower done

--- KATAs/15. Build Tower/main.py
+++ KATAs/15. Build Tower/main.py
@@ -1,3 +1,8 @@
 def validate_param(floor_number):
     if int(floor_number):
-        return True
+        return floor_number
+
+
+def tower_builder(floor_number):
+    return '\n'.join([int(n - 1) * ' ' + (2 * i - 1) * '*' + int(n - 1) * ' ' for i, n in zip(range(1, floor_number + 1), reversed(range(floor_number + 1)))])
+
--- KATAs/15. Build Tower/test.py
+++ KATAs/15. Build Tower/test.py
@@ -1,12 +1,13 @@
 import unittest as test
-from main import validate_param
+from main import validate_param, tower_builder
 
 
 class TestString(test.TestCase):
     def test_basic_test_cases(self):
-        self.assertEqual(validate_param(2), True)
-
-
+        self.assertEqual(validate_param(2), 2)
+        self.assertEqual(tower_builder(1), '*')
+        self.assertEqual(tower_builder(2), ' * ', '***')
+        self.assertEqual(tower_builder(3), '  *  ', ' *** ', '*****')
 
 
 if __name__ == '__main__':
