4fd2fa2d2125fd0b5c75c5ceef6a1eb5303512dd
Dorka <dorottya.szalai1@ibm.com>
2021-10-26 17:03:41 +0200

    build tower validation 1st test green

--- KATAs/15. Build Tower/main.py
+++ KATAs/15. Build Tower/main.py
@@ -1,6 +1,3 @@
 def validate_param(floor_number):
-
-
-
-def tower_builder(n_floors):
-    # build here
+    if int(floor_number):
+        return True
--- KATAs/15. Build Tower/test.py
+++ KATAs/15. Build Tower/test.py
@@ -1,5 +1,5 @@
 import unittest as test
-from main import validate_param, tower_builder
+from main import validate_param
 
 
 class TestString(test.TestCase):
@@ -13,13 +13,3 @@ if __name__ == '__main__':
     test.main()
 
 
-'''
-    def test_basic_test_cases(self):
-        self.assertEqual(tower_builder(1), ['*', ])
-        
-    def test_check_if_string(self):
-        self.assertEqual(tower_builder(2), [' * ', '***'])
-
-    def test_check_if_not_string(self):
-        self.assertEqual(tower_builder(3), ['  *  ', ' *** ', '*****'])
-'''
