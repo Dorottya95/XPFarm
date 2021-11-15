7d173808a808b24e01c73a42015b8c23c4c978e7
Dorka <dorottya.szalai1@ibm.com>
2021-11-15 07:32:05 +0100

    initial_board display red

--- KATAs/TicTacToe/test.py
+++ KATAs/TicTacToe/test.py
@@ -12,6 +12,17 @@ class TestClass(unittest.TestCase):
         """Check if initial board is not empty"""
         self.assertNotEqual(main.create_initial_board(), {1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'})
 
+    def test_initial_board_display(self):
+        """Check if displayed board is formatted"""
+        initial_board = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' ', 9: ' '}
+        output_board = """
+     | | 
+    -+-+-
+     | | 
+    -+-+-
+     | | """
+        self.assertEqual(output_board, main.display_board(initial_board))
+
 
 if __name__ == '__main__':
     unittest.main()
