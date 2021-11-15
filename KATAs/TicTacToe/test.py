c082ab588583dd992d5eea241bd216c1afb04f4a
Dorka <dorottya.szalai1@ibm.com>
2021-11-15 08:11:09 +0100

    check_not_win_col red

--- KATAs/TicTacToe/test.py
+++ KATAs/TicTacToe/test.py
@@ -41,6 +41,13 @@ class TestClass(unittest.TestCase):
         board = {1: 'O', 2: ' ', 3: ' ', 4: 'O', 5: ' ', 6: ' ', 7: 'O', 8: ' ', 9: ' '}
         self.assertEqual(main.check_win_col(board, "O"), "O won")
 
+    def test_check_not_win_col(self):
+        """Check if no player wins with a column of same marks"""
+        board = {1: ' ', 2: ' ', 3: ' ', 4: 'X', 5: ' ', 6: ' ', 7: 'X', 8: ' ', 9: ' '}
+        self.assertEqual(main.check_win_col(board, "X"), False)
+        board = {1: ' ', 2: 'O', 3: ' ', 4: 'X', 5: ' ', 6: ' ', 7: 'X', 8: ' ', 9: ' '}
+        self.assertEqual(main.check_win_col(board, "O"), False)
+
 
 if __name__ == '__main__':
     unittest.main()
