2ab7bfc58d4d68d89e21cf53c046254efce3cbbc
Dorka <dorottya.szalai1@ibm.com>
2021-11-15 08:23:26 +0100

    check_not_win_row red

--- KATAs/TicTacToe/test.py
+++ KATAs/TicTacToe/test.py
@@ -55,6 +55,13 @@ class TestClass(unittest.TestCase):
         board = {1: 'O', 2: 'O', 3: 'O', 4: 'X', 5: ' ', 6: ' ', 7: 'X', 8: ' ', 9: ' '}
         self.assertEqual(main.check_win_row(board, "O"), "O won")
 
+    def test_check_not_win_row(self):
+        """Check if no player wins with a row of same marks"""
+        board = {1: 'O', 2: 'O', 3: ' ', 4: 'X', 5: ' ', 6: ' ', 7: 'X', 8: ' ', 9: ' '}
+        self.assertEqual(main.check_win_row(board, "X"), False)
+        board = {1: 'O', 2: 'O', 3: ' ', 4: 'X', 5: ' ', 6: ' ', 7: 'X', 8: ' ', 9: ' '}
+        self.assertEqual(main.check_win_row(board, "O"), False)
+
 
 if __name__ == '__main__':
     unittest.main()
