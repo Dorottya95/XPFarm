867006c5cce3e36ca216e87756f936f4195bb474
Dorka <dorottya.szalai1@ibm.com>
2021-11-15 07:29:00 +0100

    initial_board creation not empty red

--- KATAs/TicTacToe/test.py
+++ KATAs/TicTacToe/test.py
@@ -8,6 +8,10 @@ class TestClass(unittest.TestCase):
         """Check if initial board is empty"""
         self.assertEqual(main.create_initial_board(), {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' ', 9: ' '})
 
+    def test_initial_board_not_be_empty(self):
+        """Check if initial board is not empty"""
+        self.assertNotEqual(main.create_initial_board(), {1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'})
+
 
 if __name__ == '__main__':
     unittest.main()
